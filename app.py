7efb3cdf2b12edc798a91d103f03bd58931c97d7
Moh Technology <141604083+mohtechnology@users.noreply.github.com>
2025-11-21 11:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-import qrcode as qr
 from flask import Flask , render_template, request, send_file
+import qrcode as qr
 
+# Defining the app
 app = Flask(__name__)
 
+# Home page how are you
 @app.route('/', methods=['GET', 'POST'])
 def gfg():
     if request.method == "POST":
+        # Defining variables 
         link = request.form.get("link")
-        qr_name = request.form.get("qr_name")
         global qr_n
-        qr_n = qr_name + ".png"
+        qr_n = ".png"
+        path = "QR-Codes"
         img = qr.make(link)
-        saved_image = img.save(qr_n)
+        saved_image = path + "/" + qr_n
+        img.save(saved_image)
         return render_template("index.html", activated=True)
     return render_template("index.html")
 
+# Image page 
 @app.route('/image')
 def hello():
-    return send_file(qr_n, mimetype='image/png')
+    return send_file("QR-Codes/" + qr_n, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(debug=True)
